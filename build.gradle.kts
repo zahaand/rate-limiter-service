1d4e137993a894a059cae30f27763485f146f542
a.zakharenkov <a.zakharenkov@mkskom.ru>
2026-05-05 01:11:13 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.3.20"
+}
+
+group = "dev.zahaand"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
